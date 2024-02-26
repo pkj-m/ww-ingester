a714d25d4dfafd5d4761e1a49cc1729cef4448fd
Pankaj Mishra <pankajmishra1511@gmail.com>
2024-02-27 04:01:15 +0530

    Add vercel config

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from fastapi import Request, FastAPI
 from database import write_event
-
+import uvicorn
 from multiprocessing import Process
 import json
 
@@ -20,3 +20,6 @@ async def ingest_event(request: Request):
     body = await request.json()
     write_event(json.dumps(body))
     return {"message": "thank you for feeding data"}
+
+if __name__ == "__main__":
+    uvicorn.run("server:app", host="0.0.0.0", port=8000)
